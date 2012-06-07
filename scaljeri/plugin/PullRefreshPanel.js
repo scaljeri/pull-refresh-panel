5e728b778e9d791f06e5fd1e8e3ccca45302c990
Lucas Calje <lucas@calje.eu>
2012-06-07 09:24:35 +0200

    added LGPL license

--- scaljeri/plugin/PullRefreshPanel.js
+++ scaljeri/plugin/PullRefreshPanel.js
@@ -1,6 +1,27 @@
 /**
- * This plugin adds pull to refresh functionality to a Panel.
+ *!
+ * Pull-refresh JavaScript Library v1.0
+ * https://github.com/scaljeri/pull-refresh-panel
  *
+ * Copyright 2012, Lucas Calje
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Date: Mon Jun 4 12:46:34 2012 +0100
+ *
+ *
+ * This plugin adds pull to refresh functionality to a Panel.
  * ## Example
  *
  */
